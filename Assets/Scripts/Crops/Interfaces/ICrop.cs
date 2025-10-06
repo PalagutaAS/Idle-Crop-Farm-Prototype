dd92f3b644e05a33f4b69c403bf87405db8eb1d0
Elrid <palagutaas@yandex.ru>
2025-10-06 20:28:56 +0500

    Refactor

--- Assets/Scripts/Crops/Interfaces/ICrop.cs
+++ Assets/Scripts/Crops/Interfaces/ICrop.cs
@@ -5,11 +5,10 @@ namespace Crops
     public interface ICrop
     {
         public CropType Type { get; }
-        
         public Vector3 Position { get; }
-        public int OnHarvest();
         public bool IsHarvesting { get; }
         public void PreparingForHarvest();
+        public int OnHarvest();
         public void Grow();
         public void Ripe();
     }
