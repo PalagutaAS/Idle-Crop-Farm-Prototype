9aee0bb06fc77fed3db41956d889a015a9ad7666
Elrid <palagutaas@yandex.ru>
2026-05-30 11:59:08 +0500

    fixed GameProgress.cs

--- Assets/Scripts/Infrastructure/SavesData/GameProgress.cs
+++ Assets/Scripts/Infrastructure/SavesData/GameProgress.cs
@@ -40,27 +40,27 @@ namespace SavesData
     [Serializable]
     public class SerializableDictionary<TKey, TValue> : Dictionary<TKey, TValue>, ISerializationCallbackReceiver
     {
-        [SerializeField] private List<TKey> _keys = new List<TKey>();
-        [SerializeField] private List<TValue> _values = new List<TValue>();
+        [SerializeField] private List<TKey> keys = new List<TKey>();
+        [SerializeField] private List<TValue> values = new List<TValue>();
 
         public void OnBeforeSerialize()
         {
-            _keys.Clear();
-            _values.Clear();
+            keys.Clear();
+            values.Clear();
             foreach (var kvp in this)
             {
-                _keys.Add(kvp.Key);
-                _values.Add(kvp.Value);
+                keys.Add(kvp.Key);
+                values.Add(kvp.Value);
             }
         }
 
         public void OnAfterDeserialize()
         {
             Clear();
-            for (int i = 0; i < _keys.Count; i++)
+            for (int i = 0; i < keys.Count; i++)
             {
-                if (i < _values.Count)
-                    Add(_keys[i], _values[i]);
+                if (i < values.Count)
+                    Add(keys[i], values[i]);
             }
         }
     }
